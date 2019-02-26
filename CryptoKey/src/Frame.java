755c700a4830ce1f8c642563b04283fd5f2a1e91
Payton Long <33674328+TheBenPayton@users.noreply.github.com>
2019-02-26 14:43:07 -0600

    Updated Frame.java

--- CryptoKey/src/Frame.java
+++ CryptoKey/src/Frame.java
@@ -25,6 +25,7 @@ public class Frame {
     //MainLoop:
     public static void main(){
         JFrame frame = new JFrame("CryptoKey V1.0");
+        frame.setResizable(false);
         JPanel panel = new JPanel(new BorderLayout());
         Container contentPane = frame.getContentPane();
         contentPane.setLayout(new FlowLayout());
@@ -34,7 +35,12 @@ public class Frame {
         JLabel label1 = new JLabel("Press submit to add key to database!");
         Checkbox one = new Checkbox("one", null, true);
         TextField tf1 = new TextField("", 25);
-        JTextArea textArea = new JTextArea();
+        JTextArea textArea = new JTextArea("**Stored Keys**\n",10, 20);
+        JScrollPane scroll = new JScrollPane(textArea);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        textArea.setEditable(false);
+        textArea.setLineWrap(true);
+        textArea.setVisible(true);
         bar.setValue(0);
         bar.setStringPainted(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -45,7 +51,7 @@ public class Frame {
         frame.getContentPane().add(tf1);
         frame.getContentPane().add(bar);
         frame.getContentPane().add(button2);
-        frame.getContentPane().add(textArea);
+        frame.getContentPane().add(scroll);
 
         //Method with stored action listener for the button:
         button.addActionListener(new ActionListener() {
@@ -81,13 +87,12 @@ public class Frame {
                 } catch (Exception ex) {
                     System.out.println("Exception: " + ex);
                 }
-
             }
         });
 
         //Code below here controls JFrame size/Style:
         frame.setSize(350, 350);
-        frame.getContentPane().setBackground(Color.cyan);
+        frame.getContentPane().setBackground(Color.lightGray);
         frame.setVisible(true);
     }
 
